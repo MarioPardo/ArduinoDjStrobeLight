d5a5168aa7c46229ec04a26e36a6bf5d1491ca57
Mario Pardo <mario.pardo003@gmail.com>
2023-08-31 15:41:14 -0400

    Initial code commit
    Still need to make some tiny tweaks

---
+++ DjLightsSketch.ino
@@ -0,0 +1,180 @@
+#include <TM1637Display.h>
+
+
+//looping buttons
+const int doublePin = 7;
+const int halfPin = 5;
+const int normalPin = 6;
+
+
+//rotary encoder
+const int rotaryBtn = 2;
+const int rotB = 3;
+const int rotA = 4;
+int rotAState = LOW;
+int rotALastState = rotAState;
+
+//display 
+const int dispDataPin = 10;
+const int dispClockPin = 16;
+uint8_t data[] = { 0xff, 0xff, 0xff, 0xff };
+TM1637Display display(dispClockPin, dispDataPin);
+
+//led output pin
+const int ledPin = 14;
+
+//variables
+float currentBPM = 126;
+float storedBPM = 126;
+int millsBetweenFlash;
+float multiplier = 1;
+int flashDuration = 50;
+bool multiplied = false;
+
+unsigned long currentMillis;
+unsigned long previousMillis;
+unsigned long buttonMillis;
+
+
+
+
+
+
+void setup() 
+{
+  pinMode(doublePin, INPUT_PULLUP);
+  pinMode(halfPin, INPUT_PULLUP);
+  pinMode(normalPin, INPUT_PULLUP);
+  pinMode(rotaryBtn, INPUT_PULLUP);
+
+  pinMode(ledPin, OUTPUT);
+
+  pinMode(rotA,INPUT_PULLUP);
+  pinMode(rotB,INPUT_PULLUP);
+  rotAState = digitalRead(rotA);
+
+  millsBetweenFlash = round(60000/currentBPM) - round(flashDuration/2);
+  currentMillis = millis();
+  previousMillis = 0;
+
+  display.setBrightness(0x0f);
+  data[0] = display.encodeDigit(0);
+  data[1] = display.encodeDigit(1);
+  data[2] = display.encodeDigit(2);
+  data[3] = display.encodeDigit(3);
+  display.setSegments(data);
+
+  display.clear();
+  display.showNumberDec(currentBPM, false);
+
+  attachInterrupt(digitalPinToInterrupt(rotB),UpdateRotary,CHANGE);
+
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  //start button
+  int startButtonState = digitalRead(rotaryBtn);
+  if(startButtonState == LOW)
+  {
+    FlashLeds();
+    delay(50);
+  }
+
+  CheckLoopingButtons();
+
+  if(multiplier == 1)
+  {
+    multiplied = false;
+    storedBPM = currentBPM;
+    flashDuration = 100;
+  }
+
+
+  millsBetweenFlash = round(60000/currentBPM) - flashDuration;
+
+  currentMillis = millis();
+  if(currentMillis - previousMillis >= millsBetweenFlash)
+  {
+    FlashLeds();
+  }
+
+  
+
+}
+
+void CheckLoopingButtons()
+{
+    int doubleButtonState = digitalRead(doublePin);
+  int halfButtonState = digitalRead(halfPin);
+  int normalButtonState = digitalRead(normalPin);
+
+  if(doubleButtonState == HIGH)
+  {
+    Serial.println("DoubleButton Pressed");
+    if(!multiplied)
+    {
+      storedBPM = currentBPM;
+      multiplied = true;
+    }
+    multiplier *= 2;
+    currentBPM *= 2;
+    flashDuration /= 2;
+    display.showNumberDec(currentBPM, false);
+    delay(150);
+  }
+   if(halfButtonState == HIGH)
+  {
+    Serial.println("HalfButton Pressed");
+     if(!multiplied)
+    {
+      storedBPM = currentBPM;
+      multiplied = true;
+    }
+    multiplier /= 2;
+    currentBPM /=2;
+    display.showNumberDec(currentBPM, false);
+    delay(150);
+  }
+   if(normalButtonState == HIGH)
+  {
+    Serial.println("NormalButton Pressed");
+    currentBPM = storedBPM;
+    multiplied = false;
+    multiplier = 1;
+    display.showNumberDec(currentBPM, false);
+    delay(150);
+  }
+}
+
+void UpdateRotary()
+{
+ rotAState = digitalRead(rotA);
+
+ if((rotALastState == LOW) && (rotAState == HIGH))
+ {
+  if(digitalRead(rotB) == HIGH) 
+  {
+    currentBPM++;
+    storedBPM = currentBPM;
+  }else
+  {
+    currentBPM--;
+    storedBPM = currentBPM;
+  }
+  display.showNumberDec(currentBPM, false);
+
+ }
+
+ rotALastState = rotAState;
+
+}
+
+void FlashLeds()
+{
+  digitalWrite(ledPin, HIGH);
+  delay(flashDuration);
+  digitalWrite(ledPin,LOW);
+  previousMillis = millis();
+}
